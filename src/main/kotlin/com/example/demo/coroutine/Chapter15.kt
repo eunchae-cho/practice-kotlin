5f9a18e827ed936ea3977af6c3dc51b1431343d1
iskra <dmsco8981@gmail.com>
2024-10-16 11:17:45 +0900

    ,

--- src/main/kotlin/com/example/demo/coroutine/Chapter15.kt
+++ src/main/kotlin/com/example/demo/coroutine/Chapter15.kt
@@ -10,17 +10,5 @@ class Chapter15 {
 }
 
 suspend fun main() {
-//    CoroutineScope(Dispatchers.Default).launch {
-//        delay(1000)
-//        printWithTread("Job 1")
-//    }
-//
-//    Thread.sleep(2000)
-
-    val job = CoroutineScope(Dispatchers.Default).launch {
-        delay(1000)
-        printWithTread("Job 1")
-    }
-
-    job.join()
+    Thread.sleep(2000)
 }
